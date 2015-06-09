09b27072cfa6f7bb0fa1e8c898cfb583f8779c2f
Krystian Duma <git@kduma.tk>
2015-06-09 19:47:05 +0200

    Styling

--- PolishValidatorRulesServiceProvider.php
+++ PolishValidatorRulesServiceProvider.php
@@ -35,18 +35,18 @@ class PolishValidatorRulesServiceProvider extends ServiceProvider {
 
 		\Validator::extend('identity_card', function($attribute, $_identity_card, $parameters)
 		{
-//sprawdz dlugosc podanego numeru
+			//sprawdz dlugosc podanego numeru
 			if(strlen($_identity_card)!=9)
 				return false;
 			$identity_card = strtoupper($_identity_card);
-//tablica wartosci znakow
+			//tablica wartosci znakow
 			$def_value = array('0'=>0,'1'=>1,'2'=>2,'3'=>3,'4'=>4,'5'=>5,'6'=>6,'7'=>7,'8'=>8,'9'=>9,
 				'A'=>10, 'B'=>11, 'C'=>12, 'D'=>13, 'E'=>14, 'F'=>15, 'G'=>16, 'H'=>17, 'I'=>18, 'J'=>19,
 				'K'=>20, 'L'=>21, 'M'=>22, 'N'=>23, 'O'=>24, 'P'=>25, 'Q'=>26, 'R'=>27, 'S'=>28, 'T'=>29,
 				'U'=>30, 'V'=>31, 'W'=>32, 'X'=>33, 'Y'=>34, 'Z'=>35);
-//tablica wag
+			//tablica wag
 			$importance = array(7,  3,  1,  0,  7,  3,  1,  7,  3);
-//oblicz sume kontrolna
+			//oblicz sume kontrolna
 			$identity_card_sum = 0;
 
 			for($i=0;$i<9;$i++){
@@ -56,7 +56,7 @@ class PolishValidatorRulesServiceProvider extends ServiceProvider {
 					return false;
 				$identity_card_sum += ((int)$def_value[$identity_card[$i]]) * $importance[$i];
 			}
-//sprawdz wartosc sumy kontrolnej
+			//sprawdz wartosc sumy kontrolnej
 			if($identity_card_sum%10 != $identity_card[3]  )
 				return false;
 			return true;
